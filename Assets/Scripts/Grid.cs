9b42144259e9dc07c8d0a0418378e92cc5097e23
Mo Afshar <moafshaar@gmail.com>
2016-11-15 17:26:19 +0000

    Fill the board

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,6 +7,7 @@ public class Grid : MonoBehaviour
 
     public enum PieceType
     {
+        EMPTY,
         NORMAL,
         //how many piecetypes there are
         COUNT,
@@ -62,26 +63,12 @@ public class Grid : MonoBehaviour
         {
             for (int y = 0; y < yDim; y++)
             {
-                GameObject newPiece = (GameObject)Instantiate(piecePrefableDict[PieceType.NORMAL], Vector3.zero, Quaternion.identity);
-                newPiece.name = "Piece(" + x + "," + y + ")";
-                newPiece.transform.parent = transform;
-
-                //storing the game piece component of the new piece game object in our pieces array
-                pieces[x, y] = newPiece.GetComponent<GamePiece>();
-                pieces[x, y].Init(x, y, this, PieceType.NORMAL);
-
-                if (pieces[x, y].IsMovable())
-                {
-                    pieces[x, y].MovableComponent.Move(x, y);
-                }
-
-                if (pieces[x, y].IsColoured())
-                {
-                    //Set it to a random colour
-                    pieces[x, y].ColourComponent.SetColour((ColourPiece.ColourType)Random.Range(0, pieces[x, y].ColourComponent.NumColours));
-                }
+                //Start empty
+                SpawnNewPiece(x, y, PieceType.EMPTY);
             }
         }
+
+        Fill();
     }
 
     // Update is called once per frame
@@ -95,4 +82,80 @@ public class Grid : MonoBehaviour
     {
         return new Vector2(transform.position.x - xDim / 2.0f + x, transform.position.y + yDim / 2.0f - y);
     }
-}
+
+    //Spawn a new piece
+    public GamePiece SpawnNewPiece(int x, int y, PieceType type)
+    {
+        GameObject newPiece = (GameObject)Instantiate(piecePrefableDict[type], GetWorldPosition(x, y), Quaternion.identity);
+        newPiece.transform.parent = transform;
+
+        //Store the game piece component in our pieces array
+        pieces[x, y] = newPiece.GetComponent<GamePiece>();
+        pieces[x, y].Init(x, y, this, type);
+
+        return pieces[x, y];
+    }
+
+    //Only move each piece by one space, true if any pieces were moved and false if not
+    public bool FillStep()
+    {
+        bool movedPiece = false;
+
+        //-2 since we don't care about the bottom row since they cant be moved down, remember 0 is at the top
+        for (int y = yDim - 2; y >= 0; y--)
+        {
+            for (int x = 0; x < xDim; x++)
+            {
+                //Get the game piece at the current position and check if it's movable
+                GamePiece piece = pieces[x, y];
+
+                if (piece.IsMovable())
+                {
+                    GamePiece pieceBelow = pieces[x, y + 1];
+
+                    if (pieceBelow.Type == PieceType.EMPTY)
+                    {
+                        //Swapping a movable piece with an empty piece
+                        piece.MovableComponent.Move(x, y + 1);
+                        pieces[x, y + 1] = piece;
+                        //Have to now make the piece above it empty since it moved down
+                        SpawnNewPiece(x, y, PieceType.EMPTY);
+                        movedPiece = true;
+                    }
+                }
+            }
+        }
+
+        //Top row is a special case, 
+        for (int x = 0; x < xDim; x++)
+        {
+            GamePiece pieceBelow = pieces[x, 0];
+
+            if (pieceBelow.Type == PieceType.EMPTY)
+            {
+                //The new piece in the -1 row, (the row above the top row)
+                GameObject newPiece = (GameObject)Instantiate(piecePrefableDict[PieceType.NORMAL], GetWorldPosition(x, -1), Quaternion.identity);
+                newPiece.transform.parent = transform;
+
+                pieces[x, 0] = newPiece.GetComponent<GamePiece>();
+                pieces[x, 0].Init(x, -1, this, PieceType.NORMAL);
+                pieces[x, 0].MovableComponent.Move(x, 0);
+                pieces[x, 0].ColourComponent.SetColour((ColourPiece.ColourType)Random.Range(0, pieces[x, 0].ColourComponent.NumColours));
+                movedPiece = true;
+            }
+        }
+
+        return movedPiece;
+    }
+
+        //Call this function until all the board is filled
+        public void Fill()
+    {
+        while (FillStep())
+        {
+
+        }
+    }
+
+    }
+
